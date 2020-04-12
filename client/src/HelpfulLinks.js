418dca165ad564dea628c4e897ee7519c80f40e6
lijeffrey39 <lijeffrey39@gmail.com>
2020-04-12 04:19:39 -0400

    resources

--- client/src/HelpfulLinks.js
+++ client/src/HelpfulLinks.js
@@ -27,31 +27,31 @@ export default function HelpfulLinks(props) {
     const defaultLinks = [
         {
             name: 'Live COVID-19 Statistics',
-            url: 'https://coronavirus.jhu.edu/map.html'
+            link: 'https://coronavirus.jhu.edu/map.html'
         },
         {
             name: 'Online support groups',
-            url: 'https://www.healingjustice.org/podcast/corona'
+            link: 'https://www.healingjustice.org/podcast/corona'
         },
         {
             name: 'Safety Practices for Mutual Aid',
-            url: 'https://mutualaiddisasterrelief.org/wp-content/uploads/2020/03/COVID-SupplyDistro-MASafetyPracticesZine-WEB.pdf'
+            link: 'https://mutualaiddisasterrelief.org/wp-content/uploads/2020/03/COVID-SupplyDistro-MASafetyPracticesZine-WEB.pdf'
         },
         {
             name: 'COVID-19 Prevention Information',
-            url: 'https://docs.google.com/document/d/1y5ST-wUZ6ASh-j6pQ7zmdwEsJIHI0MjtL97aGb7awmM/edit?fbclid=IwAR17xA1fmLUY9faEhPFS5l5JyKN76vynIY-5SZh6e8Iz6Uqa69vAXdKeQcs'
+            link: 'https://docs.google.com/document/d/1y5ST-wUZ6ASh-j6pQ7zmdwEsJIHI0MjtL97aGb7awmM/edit?fbclid=IwAR17xA1fmLUY9faEhPFS5l5JyKN76vynIY-5SZh6e8Iz6Uqa69vAXdKeQcs'
         },
         {
             name: 'Accessible teaching',
-            url: 'https://www.mapping-access.com/blog-1/2020/3/10/accessible-teaching-in-the-time-of-covid-19'
+            link: 'https://www.mapping-access.com/blog-1/2020/3/10/accessible-teaching-in-the-time-of-covid-19'
         },
         {
             name: 'Mental health in the face of uncertainty',
-            url: 'https://afsp.org/taking-care-of-your-mental-health-in-the-face-of-uncertainty/'
+            link: 'https://afsp.org/taking-care-of-your-mental-health-in-the-face-of-uncertainty/'
         },
         {
             name: 'EPA List of disinfectants that kill coronavirus',
-            url: 'https://www.epa.gov/pesticide-registration/list-n-disinfectants-use-against-sars-cov-2'
+            link: 'https://www.epa.gov/pesticide-registration/list-n-disinfectants-use-against-sars-cov-2'
         }
     ]
 
@@ -76,7 +76,7 @@ export default function HelpfulLinks(props) {
                         return <ListGroup.Item key={i} style = {{fontSize: 12}}>
                                 <Row>
                                     <Col><a target="_blank" 
-                                            href={link.url}
+                                            href={link.link}
                                             rel="noopener noreferrer">{link.name}</a></Col>
                                 </Row>
                             </ListGroup.Item>      
@@ -106,7 +106,7 @@ export default function HelpfulLinks(props) {
                     return <ListGroup.Item key={i} style = {{fontSize: 12}}>
                             <Row>
                                 <Col><a target="_blank" 
-                                        href={link.url}
+                                        href={link.link}
                                         rel="noopener noreferrer">{link.name}</a></Col>
                             </Row>
                         </ListGroup.Item>      
