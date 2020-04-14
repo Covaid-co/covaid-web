9e2572c49b0329f0679ddaf509e75ab98d32622a
debanikpurkayastha <debanik1997@gmail.com>
2020-04-14 19:18:17 -0400

    setting up edit modal

--- client/src/EditAccountInfoModal.js
+++ client/src/EditAccountInfoModal.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Modal from 'react-bootstrap/Modal'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function EditAccountInfoModal(props) {
 
