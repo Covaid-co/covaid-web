62fde2ea900eaa235d0b5aaf35e81114b3182399
debanikpurkayastha <debanik1997@gmail.com>
2020-05-06 15:32:56 -0400

    added spanish localization

--- client/src/association_request_headers/OrgHeader.js
+++ client/src/association_request_headers/OrgHeader.js
@@ -1,33 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {translations} from './translations';
+import LocalizedStrings from 'react-localization';
+
+let translatedStrings = new LocalizedStrings({translations});
 
 export default function OrgHeader(props) {
+    const [language, setLanguage] = useState('es');
+    const [loadedStrings, setLoadedStrings] = useState(false);
+
+    useEffect(() => {
+        setLoadedStrings(true);
+    });
+
+    if (!loadedStrings) {
+        return <></>;
+    }
+
+    const createFormName = (assocName, assocCity) => {
+        switch (language) {
+            case 'en':
+                return assocName + " " + translatedStrings[language].formTitle
+            case 'es':
+                return "Formulario de Ayuda Mutua en " + assocCity;
+            default:
+                break;
+        }
+    }
+
     return (
         <>
-            <h1 id="small-header">{props.assoc.name} Form</h1>
+            <h1 id="small-header">{createFormName(props.assoc.name, props.assoc.city)}</h1>
             <p id="regular-text">
-                If you need support or have resources to offer as we navigate the COVID-19 crisis as a community, 
-                please use this form. Your responses to this form will only be seen by a team of volunteers working 
-                to match resources with needs. You can find more public resources at 
+                {translatedStrings[language].intro}
                 <a href={props.assoc.homepage} target="_blank" rel="noopener noreferrer"> {props.assoc.name}</a>.
             </p>
             <p id="regular-text">
-                We are prioritizing folks who are sick, disabled, quarantined without pay, elderly, undocumented, 
-                queer, Black, Indigenous, and/or people of color -- including those displaced from {props.assoc.city} to the nearby areas. 
+                {translatedStrings[language].prioritizing} {props.assoc.city} {translatedStrings[language].areas}. 
             </p>
             <p id="regular-text">
-                This form is being managed by a group of all-volunteer {props.assoc.city} community members involved in local organizing efforts. 
-                Thank you for your patience and grace. 
+                {translatedStrings[language].formManage} {props.assoc.city} {translatedStrings[language].managedBy}. 
             </p>
             <p id="regular-text">
-                If you have questions or need help filling out this form, please contact:<br/>
+                {translatedStrings[language].questions}:<br/>
                 <strong>{props.assoc.email}</strong>
             </p>
             <p id="regular-text">
-                Let’s take care of each other. By us, for us. <br/>
-                Vamos a cuidarnos unes a otres. Por nosotres, para nosotres. 
+                {translatedStrings[language].motto}
             </p>
-            <p id="request-calling" style={{borderBottom: '0px solid', marginBottom: 0}}> For those who would rather call in a request, 
-                please call <br /><span id="phoneNumber">{props.assoc.phone ? props.assoc.phone : '(401) 526-8243'}</span></p>
+            <p id="request-calling" style={{borderBottom: '0px solid', marginBottom: 0}}>{translatedStrings[language].call}: 
+            <br /><span id="phoneNumber">{props.assoc.phone ? props.assoc.phone : '(401) 526-8243'}</span></p>
         </>
     );
 }
---
+++ client/src/association_request_headers/translations.js
@@ -0,0 +1,25 @@
+const translations = {
+    en:{
+        intro: "If you need support or have resources to offer as we navigate the COVID-19 crisis as a community, please use this form. Your responses to this form will only be seen by a team of volunteers working to match resources with needs. You can find more public resources at ",
+        prioritizing: "We are prioritizing folks who are sick, disabled, quarantined without pay, elderly, undocumented, queer, Black, Indigenous, and/or people of color -- including those displaced from",
+        areas: "to the nearby areas",
+        formManage: "This form is being managed by a group of all-volunteer",
+        managedBy: "community members involved in local organizing efforts. Thank you for your patience and grace.",
+        questions: "If you have questions or need help filling out this form, please contact:",
+        motto: "Let’s take care of each other. By us, for us.",
+        call: "For those who would rather call in a request, please call",
+        formTitle: "Form"
+    },
+    es: {
+        intro: "Si usted necesita ayuda o tiene recursos para ofrecer mientras que navegamos como una comunidad el crisis de COVID-19, por favor rellenar el formulario. Sus respuestas al formulario solo serían vistos por un equipo de voluntarias/os trabajando para juntar recursos con necesidades. Usted puede encontrar más recursos públicos en ",
+        prioritizing:"Estamos priorizando personas quienes están enfermos, tiene algún discapacidad, estan en cuarentena sin pago, personas mayores, personas indocumentados, personas queer, Negro, Indigena, y/o personas de color-- incluyendo personas desplazadas de ",
+        areas: "a lugares cercanos",
+        formManage: "Este formulario está siendo manejado por un grupo de voluntarias/os de la comunidad de",
+        managedBy: "quienes están involucrados en movimientos sociales a nivel local. Gracias por su paciencia y cortesía.",
+        questions: "Si tiene preguntas o necesita ayuda rellenando este formulario, por favor contactar:",
+        motto: "Vamos a cuidarnos unes a otres. Por nosotres, para nosotres.",
+        call: "Por las personas que prefieren llamar con una solicitud, por favor llaman",
+        formTitle: "Formulario de Ayuda Mutua en"
+    }
+  }
+  export {translations};
