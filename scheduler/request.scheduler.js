387abfe0594724cf3401530455bbd53fa3a18e18
lijeffrey39 <lijeffrey39@gmail.com>
2020-05-08 18:14:54 -0400

    schedule on prod

--- scheduler/request.scheduler.js
+++ scheduler/request.scheduler.js
@@ -16,14 +16,16 @@ const Association = require('../models/association.model')
 const Users = require('../models/user.model')
  
 exports.request_scheduler = function() {
-    cron.schedule("0 */30 * * * *", function() {
-        updateAllExpiredRequests()
-    })
-
-    cron.schedule("0 0 */12 * * *", function() {
-        console.log("reminding")
-        remindPendingRequests()
-    })
+    if (process.env.PROD) {
+        cron.schedule("0 */30 * * * *", function() {
+            updateAllExpiredRequests();
+        });
+    
+        cron.schedule("0 0 */12 * * *", function() {
+            console.log("reminding")
+            remindPendingRequests();
+        });
+    }
 };
 
 const remindPendingRequests = () => {
