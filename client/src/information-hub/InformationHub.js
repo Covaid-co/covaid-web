213cbd23de358d20f6d51f4a94fe328b0db57b36
debanikpurkayastha <debanik1997@gmail.com>
2020-05-27 09:45:16 -0400

    added some comments to InformationHub.js

--- client/src/information-hub/InformationHub.js
+++ client/src/information-hub/InformationHub.js
@@ -44,6 +44,29 @@ export default function InformationHub() {
         <Col md={2}>
         </Col>
       </Row>
+      
+      {/* 
+
+        You'll want to make a component called InformationSection that takes in 2 props
+        -> sectionID (will be used inside component to fetch all resources related to that sectionID)
+          -> to see code examples of this look at the useEffect method inside YourOFfer.js (lines 57 - 66)
+          -> Basically that will make an API call to your API
+          -> Have a state variable (look at usage of useState) -> const [resources, setResources] = useState([]);
+          -> After the fetch, setResources(<response from backend>)
+        -> sectionName (will be used to render the name of the section)
+        
+        You can then make 4 of them down below
+
+      <Row>
+        <Col md={2}>
+        </Col>
+        <Col id="login-container">
+            <InformationSection sectionID={1} sectionName={"The Basics of Coronavirus"} />
+        </Col>
+        <Col md={2}>
+        </Col>
+      </Row> 
+      */}
 
 
     </Container>
